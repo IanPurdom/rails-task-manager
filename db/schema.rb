1f11f6744d05041162c04bbcd9fb2abadda3ab2b
Yan de Catheu <ianpurdom75@gmail.com>
2017-11-21 15:00:39 +0100

    task done

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171121102232) do
+ActiveRecord::Schema.define(version: 20171121113504) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
-    t.integer "details"
+    t.string "details"
     t.boolean "completed", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
